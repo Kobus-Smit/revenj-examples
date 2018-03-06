043110e11b6168e0014f33788f5a64b229314b32
Kobus Smit <ksmit.aus@gmail.com>
2018-03-06 18:08:24 +1100

    Initial commit

---
+++ SimpleMapping/Common/Src/DSL/SimpleMapping.dsl
@@ -0,0 +1,22 @@
+﻿module SimpleMapping {
+
+root Person {
+	string Name;
+}
+
+struct PersonVM {
+	string Name;
+	has mapping ToVM from Person;
+	has mapping FromVM into Person;
+}
+
+struct CommonParam
+{
+	PersonVM Person;
+	bool Other1;
+}
+
+mapping ToVM { identical; }  
+mapping FromVM { identical; }  
+  
+}
---
+++ SimpleMapping/Front-end/Src/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Revenj;
+
+namespace SimpleMapping.App.WinForms
+{
+    public partial class MainForm : Form
+    {
+        private readonly IServiceProvider provider;
+        private readonly IStandardProxy standardProxy;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            provider = Client.Start("http://localhost:8999/", null);
+            standardProxy = provider.Resolve<IStandardProxy>();
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            var cp = new CommonParam { Person = new PersonVM { Name = "Aaaa" }, Other1 = true };
+            var result = standardProxy.Execute<CommonParam, string>("SimpleMapping.App.Service.Plugin.TestCommonParameter", cp).Result;
+
+            var person = new Person { Name = "Bbbb" }.Create();
+            
+            //How do I get this working?
+            Common.Test.Abc(person);
+        }
+    }
+}
