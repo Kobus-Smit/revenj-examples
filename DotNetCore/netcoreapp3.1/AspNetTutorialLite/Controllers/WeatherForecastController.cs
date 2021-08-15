58cc393c79685f21a423150c1db22e140160b777
Kobus-Smit <ksmit.aus@gmail.com>
2021-08-15 12:33:41 +1000

    Added AspNetTutorialLite for .NET Core 3.1 and .NET 5.0

---
+++ DotNetCore/netcoreapp3.1/AspNetTutorialLite/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetTutorial.Customers;
+using Microsoft.AspNetCore.Mvc;
+using tutorial;
+
+namespace AspNetTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private readonly ICustomerService customerService;
+
+        public WeatherForecastController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        // GET
+        public Customer Index()
+        {
+            return customerService.Lookup("b7e5a8bfd7864c0");;
+        }
+    }
+}
