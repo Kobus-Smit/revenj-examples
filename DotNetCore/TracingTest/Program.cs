bdeeb143acaea0d39c38c080fb75c11f63135406
Kobus-Smit <ksmit.aus@gmail.com>
2021-10-24 11:56:33 +1100

    Add TracingTest

---
+++ DotNetCore/TracingTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+            Trace.AutoFlush = true;
+            var serilogTraceListener = new SerilogTraceListener.SerilogTraceListener();
+            Trace.Listeners.Add(serilogTraceListener);
+            Trace.WriteLine("A string written using Trace.WriteLine");
+
+            var source = new TraceSource("Test");
+            source.Switch.Level = SourceLevels.All;  //TODO:KS It needs this...
+            source.Listeners.Add(serilogTraceListener); //TODO:KS It needs this...
+            source.TraceEvent(TraceEventType.Information, 1001, "os={0}", Environment.OSVersion);
+
+            Console.ReadLine();
+        }
+
+    }
+}
